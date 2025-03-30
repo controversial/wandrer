65d77d381e986082699f4f565c083854dff96732
Luke Taylor <luke@deentaylor.com>
2025-03-30 16:00:03 -0400

    pbf parsing for tile-data

---
+++ src/app/data.ts
@@ -0,0 +1,46 @@
+import Pbf from 'pbf';
+
+function readTileDataInner(pbf: Pbf, end: number) {
+  return pbf.readFields((tag, result) => {
+    switch (tag) {
+      case 1:
+        result.osm_id = pbf.readVarint(true);
+        break;
+      case 2:
+        result.timestamp = pbf.readVarint();
+        break;
+      case 3:
+        result.is_bike = pbf.readBoolean();
+        break;
+      case 4:
+        result.is_combined = pbf.readBoolean();
+        break;
+      default:
+        console.warn('unexpected tag in tile data', tag);
+        break;
+    }
+  }, {
+    osm_id: 0,
+    timestamp: 0,
+    is_bike: false,
+    is_combined: false,
+  }, end);
+}
+
+export function readTileData(buff: ArrayBuffer) {
+  const pbf = new Pbf(buff);
+  return pbf.readFields(
+    (tag, result) => {
+      if (tag === 1) {
+        const dataLength = pbf.readVarint();
+        const dataPoint = readTileDataInner(pbf, pbf.pos + dataLength);
+        if (dataPoint.is_bike) {
+          result[dataPoint.osm_id.toString()] = dataPoint.timestamp * 1000;
+        }
+      } else {
+        console.warn('unexpected tag in tile data', tag);
+      }
+    },
+    {} as Record<string, number>,
+  );
+}
