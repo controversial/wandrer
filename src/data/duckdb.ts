c497365766ed685c031ca7465f308e0ab23d783b
Luke Taylor <luke@deentaylor.com>
2025-04-26 17:50:35 -0400

    load duckdb

---
+++ src/data/duckdb.ts
@@ -0,0 +1,34 @@
+import * as duckdb from '@duckdb/duckdb-wasm';
+import duckdbWasm from '@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm';
+import duckdbWasmNext from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm';
+
+
+if (typeof window === 'undefined') throw new Error('DuckDB must be lazy-loaded on the client');
+
+
+// Instantiate a DuckDB instance
+
+const BUNDLES = {
+  mvp: {
+    mainModule: duckdbWasm,
+    mainWorker: new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js', import.meta.url).toString(),
+  },
+  eh: {
+    mainModule: duckdbWasmNext,
+    mainWorker: new URL('@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js', import.meta.url).toString(),
+  },
+} satisfies duckdb.DuckDBBundles;
+
+const bundle = await duckdb.selectBundle(BUNDLES);
+if (!bundle.mainWorker) throw new Error('No worker URL found in the selected bundle.');
+const worker = new Worker(bundle.mainWorker);
+const logger = new duckdb.ConsoleLogger();
+const db = new duckdb.AsyncDuckDB(logger, worker);
+
+// Load the “spatial” extension
+
+await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+const conn = await db.connect();
+await conn.query('INSTALL spatial; LOAD spatial;');
+
+export { db, conn };
