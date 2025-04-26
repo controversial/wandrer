232627d34cd41a4b222838592d6fbdc07a2480d8
Luke Taylor <luke@deentaylor.com>
2025-04-26 15:21:46 -0400

    Lift layer definitions out of Map component body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useControls, Leva } from 'leva';
 
 import MapboxMap from 'components/Map';
 import { readTileData } from './data';
-import type { LayerSpecification } from 'mapbox-gl';
+import type { SourceSpecification, LayerSpecification } from 'mapbox-gl';
 
 import classNames from 'classnames/bind';
 import styles from './page.module.scss';
@@ -13,6 +13,64 @@ const cx = classNames.bind(styles);
 
 if (!process.env.NEXT_PUBLIC_WANDRER_ATHLETE_ID) throw new Error('Missing athlete ID');
 const WANDRER_ATHLETE_ID = process.env.NEXT_PUBLIC_WANDRER_ATHLETE_ID;
+
+
+const sources = [
+  // used for “traveled” segments; supports CORS
+  {
+    id: 'wandrer-1',
+    type: 'vector',
+    tiles: [`https://tiles2.wandrer.earth/tiles/${WANDRER_ATHLETE_ID}/bike/{z}/{x}/{y}`],
+    maxzoom: 13,
+  },
+  // used for “untraveled” segments; proxied through an API route due to no CORS support
+  {
+    type: 'vector',
+    id: 'wandrer-2',
+    tiles: ['/api/tiles/untraveled/{z}/{x}/{y}'],
+    maxzoom: 13,
+  },
+] satisfies SourceSpecification[];
+
+const layers = (
+  { traveledColor, untraveledColor }: { traveledColor: string; untraveledColor: string; },
+) => [
+  // untraveled segments in red
+  {
+    id: 'wandrer-untraveled',
+    type: 'line',
+    source: 'wandrer-2',
+    'source-layer': 'missing_segments',
+    slot: 'middle',
+    paint: {
+      'line-color': untraveledColor,
+      'line-occlusion-opacity': 0.3,
+      'line-width': {
+        type: 'exponential',
+        base: 1.5,
+        stops: [[11, 1], [16, 4]],
+      },
+    },
+  },
+
+  // traveled segments in blue
+  {
+    id: 'wandrer-traveled',
+    type: 'line',
+    source: 'wandrer-1',
+    'source-layer': 'se',
+    slot: 'middle',
+    paint: {
+      'line-color': traveledColor,
+      'line-occlusion-opacity': 0.3,
+      'line-width': {
+        type: 'exponential',
+        base: 1.5,
+        stops: [[11, 1], [16, 4]],
+      },
+    },
+  },
+] satisfies LayerSpecification[];
 
 
 export default function Page() {
@@ -48,60 +106,11 @@ export default function Page() {
   return (
     <div className={cx('base')}>
       <MapboxMap
-        sources={useMemo(() => [
-          // used for “traveled” segments; supports CORS
-          {
-            id: 'wandrer-1',
-            type: 'vector',
-            tiles: [`https://tiles2.wandrer.earth/tiles/${WANDRER_ATHLETE_ID}/bike/{z}/{x}/{y}`],
-            maxzoom: 13,
-          },
-          // used for “untraveled” segments; proxied through an API route due to no CORS support
-          {
-            type: 'vector',
-            id: 'wandrer-2',
-            tiles: ['/api/tiles/untraveled/{z}/{x}/{y}'],
-            maxzoom: 13,
-          },
-        ], [])}
-
-        layers={useMemo(() => [
-          // untraveled segments in red
-          {
-            id: 'wandrer-untraveled',
-            type: 'line',
-            source: 'wandrer-2',
-            'source-layer': 'missing_segments',
-            slot: 'middle',
-            paint: {
-              'line-color': untraveledColor,
-              'line-occlusion-opacity': 0.3,
-              'line-width': {
-                type: 'exponential',
-                base: 1.5,
-                stops: [[11, 1], [16, 4]],
-              },
-            },
-          },
-
-          // traveled segments in blue
-          {
-            id: 'wandrer-traveled',
-            type: 'line',
-            source: 'wandrer-1',
-            'source-layer': 'se',
-            slot: 'middle',
-            paint: {
-              'line-color': traveledColor,
-              'line-occlusion-opacity': 0.3,
-              'line-width': {
-                type: 'exponential',
-                base: 1.5,
-                stops: [[11, 1], [16, 4]],
-              },
-            },
-          },
-        ] satisfies LayerSpecification[], [traveledColor, untraveledColor])}
+        sources={sources}
+        layers={useMemo(
+          () => layers({ traveledColor, untraveledColor }),
+          [traveledColor, untraveledColor],
+        )}
       />
 
       <Leva collapsed />
