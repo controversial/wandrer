d72380006b833cd54cfb3df4dd37caa682743e7e
Luke Taylor <luke@deentaylor.com>
2025-05-31 18:38:02 -0400

    Begin notebook downloading OSM network

---
+++ data/scratch/map-matching.py
@@ -0,0 +1,122 @@
+import marimo
+
+__generated_with = "0.13.14"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    _base_excludes = (
+        '["area"!="yes"]'
+        '["access"!="no"]'
+        # no destroyed roads
+        '[!"razed"][!"razed:highway"][!"demolished:highway"][!"removed:highway"]'
+        # rail/water/indoor are inacccessible by bike
+        '[!"railway"]'
+        '[!"waterway"]'
+        '["route"!="ferry"]'
+        '["indoor"!="yes"]'
+        # practically untraversable routes
+        '["mtb:scale"!="6"]'
+    )
+    _excludes_ignoring_bicycle_tag = (
+        # Exclude limited access roads
+        '["access"!~"^(private|customers|military|delivery|emergency|no)$"]'
+        # no big highways
+        '["motorroad"!="yes"]'
+        # exclude golf courses
+        '["golf_cart"!~"^(yes|designated|private)$"]'
+        # practically untraversable routes
+        '["mtb:scale"!="6"]'
+        # no tunnels - although this has some false positives for short tunnels that are
+        # in fact part of bikeable roads
+        # TODO: postprocess filter tunnels by length instead?
+        # '["tunnel"!~"^(yes|building_passage)$"]'
+    )
+    _bicycle_tag_excludes = '["bicycle"!~"^(dismount|use_sidepath|private|no)$"]'
+    excludes = _base_excludes + _excludes_ignoring_bicycle_tag + _bicycle_tag_excludes
+
+
+    # 1. Start with road types that can be *assumed* to have bicycle access,
+    #    unless otherwise specified.
+    assumed_bike_access = [
+        (
+            '["highway"]'
+            '["highway"!~"^(motorway|motorway_link|steps|stairs|escalator|elevator|construction|proposed|demolished|escape|bus_guideway|platform|elevator|raceway|rest_area|traffic_island|services|yes|no|razed|corridor|busway|via_ferrata)$"]'
+            '["highway"!~"^(trunk|trunk_link|footway|service|bridleway)$"]'
+            '["highway"!="pedestrian"]'
+            f"{excludes}"
+        ),
+        ## **Named** service roads that don’t have a specific service=... tag
+        f'["highway"="service"]["name"][!"service"]{excludes}',
+    ]
+
+    # 2. Add in ways that have bicycle access explicitly marked
+    explicit_bike_access = [
+        # Ways with allowed values in bicycle=... tags
+        f'["highway"]["bicycle"~"^(yes|designated|permissive|official|mtb|MTB)$"]{_base_excludes}',
+        # Ways with bicycle:designated... tags
+        f'["highway"][~"^bicycle:designated.*$"~"."]{excludes}',
+        # Ways with a `highway` tag of cycleway
+        f'["highway"="cycleway"]{excludes}',
+        # Ways marked with bicycle_road/cyclestreet
+        f'["highway"][~"^(bicycle_road|cyclestreet)$"~"."]{excludes}',
+        # Ways with `bicycle:no` but `bicycle:conditional`
+        f'["highway"]["bicycle:conditional"~"^yes"]{_excludes_ignoring_bicycle_tag}',
+        # Ways that are part of relations with route=bicycle
+        # We do some Overpass query injection to query for relations
+        # f'(1)(2);relation["route"="bicycle"]'
+    ]
+
+    bike_filter = assumed_bike_access + explicit_bike_access
+    return (bike_filter,)
+
+
+@app.cell
+def _(bike_filter):
+    import osmnx as ox
+    import osmnx.settings
+
+    osmnx.settings.useful_tags_way = ["name", "highway", "access", "oneway", "ref", "bicycle"]
+
+    bike_network_directed = ox.simplify_graph(
+        ox.graph_from_place(
+            "Manhattan, New York, USA",
+            custom_filter=bike_filter,
+            simplify=False,
+            retain_all=True,
+            truncate_by_edge=True,
+        ),
+        edge_attrs_differ=["osmid"],
+    )
+    bike_network_undirected = ox.convert.to_undirected(bike_network_directed)
+    return bike_network_undirected, ox
+
+
+@app.cell
+def _(bike_network_undirected, ox):
+    ways_gdf = ox.convert.graph_to_gdfs(bike_network_undirected, nodes=False)
+    ways_gdf["osmid"] = ways_gdf["osmid"].astype(str)
+    ways_gdf = ways_gdf.set_index("osmid")
+    ways_gdf
+    return (ways_gdf,)
+
+
+@app.cell
+def _(ways_gdf):
+    import leafmap.foliumap as leafmap
+
+    map = leafmap.Map()
+    map.add_basemap("CartoDB.DarkMatter")
+    map.add_gdf(
+        ways_gdf.reset_index(),
+        layer_name="Ways",
+        info_mode="on_click",
+        style={"color": "#ffffff", "weight": 2, "fill": False, "opacity": 0.5}
+    )
+    map
+    return
+
+
+if __name__ == "__main__":
+    app.run()
