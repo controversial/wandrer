74d6be3539e864d1e0496c44e62a90c7a07fa7f7
Luke Taylor <luke@deentaylor.com>
2025-12-28 01:58:59 -0500

    modernize eslint-plugin-next

--- eslint/next.ts
+++ eslint/next.ts
@@ -1,44 +1,7 @@
 import { defineConfig } from 'eslint/config';
-import { fixupPluginRules } from '@eslint/compat';
-import globals from 'globals';
-
 import nextPlugin from '@next/eslint-plugin-next';
 
-
-export default defineConfig({
-  name: 'eslint-config-next (recreated)',
-
-  plugins: {
-    '@next/next': fixupPluginRules({ rules: nextPlugin.rules }),
-  },
-
-  rules: {
-    'import/no-anonymous-default-export': 'warn',
-    'react/no-unknown-property': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/alt-text': ['warn', { elements: ['img'], img: ['Image'] }],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
-    'react/jsx-no-target-blank': 'off',
-
-    ...nextPlugin.configs.recommended.rules,
-    ...nextPlugin.configs['core-web-vitals'].rules,
-  },
-
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-
-  languageOptions: {
-    globals: {
-      ...globals.node,
-      ...globals.browser,
-    },
-  },
-});
+export default defineConfig(
+  nextPlugin.configs.recommended,
+  nextPlugin.configs['core-web-vitals'],
+);
