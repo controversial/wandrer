183c0ed5014972fbe53e64dfc60f53ef561d3a0a
Luke Taylor <luke@deentaylor.com>
2025-12-28 02:00:37 -0500

    slight cleanup of eslint config entrypoint

--- eslint/index.ts
+++ eslint/index.ts
@@ -8,7 +8,7 @@ import js from '@eslint/js';
 
 // Configs
 import airbnbConfig from './airbnb/index.js';
-import eslintConfigNext from './next.js';
+import nextConfig from './next.js';
 import reactConfig from './react.js';
 import jsxStylisticConfig from './jsx-stylistic.js';
 import importConfig from './import.js';
@@ -23,73 +23,54 @@ export default defineConfig(
   {
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
-      parserOptions: { sourceType: 'module', ecmaVersion: 'latest' },
+      parser: tseslint.parser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        projectService: true,
+        tsconfigRootDir: path.dirname(import.meta.dirname),
+      },
     },
   },
 
   // recommended configs
   js.configs.recommended,
-  ...reactConfig,
-  ...importConfig,
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
 
   // airbnb rules
-  ...airbnbConfig,
-  // nextjs rules
-  ...eslintConfigNext,
-  // jsx stylistic rules
-  ...jsxStylisticConfig,
-
+  airbnbConfig,
+  reactConfig,
+  importConfig,
+  nextConfig,
+  jsxStylisticConfig,
 
   // Luke overrides rules
   {
     rules: {
-      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],   // Allow importing devDependencies
-      'no-param-reassign': ['error', { props: false }],                            // Allow modifying properties of parameters
       'no-underscore-dangle': 'off',                                               // Allow names like _a
-      'global-require': 'off',                                                     // Allow require() in local scope
-      '@stylistic/object-curly-newline': ['error', { consistent: true }],          // Either both braces have newlines or neither does
-      'import/no-webpack-loader-syntax': 'off',                                    // Allow webpack loader syntax (with !)
-      'import/newline-after-import': 'off',                                        // Allow code right after import statements
-      'import/extensions': 'off',                                                  // Allow importing modules without specifying extensions
-      'import/order': 'off',                                                       // Allow importing modules in any order
-      'jsx-a11y/anchor-is-valid': 'off',                                           // In Next.js the href goes on the Link wrapper, not on the a tag
-      'react/no-danger': 'off',                                                    // Allow using dangerouslySetInnerHTML
-      'react/jsx-fragments': 'off',                                                // Allow either React Fragment syntax
-      'react/jsx-props-no-spreading': 'off',                                       // Allow spreading props (like <Component {...props} />)
-      'react/no-array-index-key': 'off',                                           // Allow using index as key in array
-      '@stylistic/no-multiple-empty-lines': ['error', { max: 2, maxBOF: 0, maxEOF: 0 }], // Allow up to 2 empty lines in a row
       'no-empty': ['error', { allowEmptyCatch: true }],                            // Allow empty blocks when it's for a catch statement
-      'react/forbid-prop-types': 'off',                                            // Allow vague prop-types like “object”
-      'react/prop-types': 'off',
       'prefer-const': ['error', { destructuring: 'all' }],                         // For destructured declarations where only some variables get reassigned, don’t require const for the others
-      'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],    // Allow associating form labels by nesting
       'no-continue': 'off',                                                        // Allow continue statements
       'no-restricted-exports': ['error', { restrictedNamedExports: ['then'] }],    // Allow `export { default }` syntax
-      'import/prefer-default-export': 'off',                                       // Allow modules to have a single named export
-      '@stylistic/lines-between-class-members': 'off',                             // Allow omitting an empty line between class members
       'max-classes-per-file': 'off',                                               // Allow more than one class per file
-      'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }], // Allow JSX in .tsx files
       'no-nested-ternary': 'off',                                                  // Allow nested ternary expressions
+
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],   // Allow importing devDependencies
+      'import/no-webpack-loader-syntax': 'off',                                    // Allow webpack loader syntax (with !)
+      'import/newline-after-import': 'off',                                        // Allow code right after import statements
+      'import/order': 'off',                                                       // Allow importing modules in any order
+      'import/extensions': 'off',                                                  // Allow importing modules without specifying extensions
       'import/no-anonymous-default-export': ['error', { allowObject: true, allowArray: true }],
       'import/no-named-as-default': 'off',                                         // too many false positives for packages like react, zod, classNames - https://github.com/import-js/eslint-plugin-import/issues/1594
-      'no-spaced-func': 'off',                                                     // deprecated, and broken in typescript
+      'import/prefer-default-export': 'off',                                       // Allow modules to have a single named export
+
+      '@stylistic/object-curly-newline': ['error', { consistent: true }],          // Either both braces have newlines or neither does
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 2, maxBOF: 0, maxEOF: 0 }], // Allow up to 2 empty lines in a row
+      '@stylistic/lines-between-class-members': 'off',                             // Allow omitting an empty line between class members
       // '@stylistic/indent-binary-ops': ['error', 2],
       '@stylistic/type-generic-spacing': 'error',
       '@stylistic/type-named-tuple-spacing': 'error',
-      // use default arguments instead of defaultProps for function components
-      'react/require-default-props': [
-        'error',
-        { forbidDefaultForRequired: true, classes: 'defaultProps', functions: 'ignore' },
-      ],
-      'no-restricted-syntax': [
-        'error',
-        { selector: 'ForInStatement', message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.' },
-        // Removed: { selector: 'ForOfStatement', message: 'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.' },
-        { selector: 'LabeledStatement', message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.' },
-        { selector: 'WithStatement', message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.' },
-      ],
       '@stylistic/indent': ['error', 2, {
         SwitchCase: 1,
         VariableDeclarator: 1,
@@ -114,14 +95,6 @@ export default defineConfig(
 
   // typescript overrides
   {
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: path.dirname(import.meta.dirname),
-      },
-    },
-
     rules: {
       // Rules specific to typescript
       '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
